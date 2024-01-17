f00590644455f3e7f3c2981ad9f3edaac4ee2cba
Dustin Dannenhauer <dustin.td@gmail.com>
2024-01-17 21:23:04 +0000

    test code for db connection

--- scripts/get_DVM_events_from_relays.py
+++ scripts/get_DVM_events_from_relays.py
@@ -59,7 +59,6 @@ def write_events_to_db(events):
             print(f"Error inserting events into database: {e}")
 
 
-
 import threading
 import time
 from threading import Lock
@@ -163,28 +162,28 @@ if __name__ == "__main__":
         raise FileNotFoundError(f".env file not found at {env_path} ")
 
     ### TEST CODE TO PUT AN EVENT INTO PYMONGO
-    # test_event_1 = {
-    #     "id": "a5b9b081a6d8f43581288ab8ec25f3d670ab77c9dfed3c9e5673fd52571b8794",
-    #     "pubkey": "c63c5b4e21b9b1ec6b73ad0449a6a8589f6bd8542cabd9e5de6ae474b28fe806",
-    #     "created_at": 1705189941,
-    #     "kind": 5100,
-    #     "tags": [
-    #         ["p", "89669b03bb25232f33192fdda77b8e36e3d3886e9b55b3c74b95091e916c8f98"],
-    #         ["encrypted"],
-    #     ],
-    #     "content": "k84/yUywhNvWhDUOhxC/dWIb+w5R6KybLl0BpykYcDqY0ky7sJC4rnbxfsKvMjh1zIexyQ75M+NRxqA/JnBEekPw93xu6TfqtH/tVThbyOWG0fn77ptM1aEPziI52kwbM0hrSbtsJxKEm9LfWvF7+s9yv4/pqIkYMiMZ56JqxG/XH+3DxXoa0ZJQr1GRBpfsb1h8WMA5EqmEOB/OSKyeA9J7/7/U4ONHcsBc0yTPCBJ6pnTlgF19scwAvGXw9F1sG9gc/65nzvsfjaQ4venFh0A9QpH9f4/02qyBnOTcmTs0RIT9etyEZQeBoJlJL9+Tu1OD6sp66hbZ6GDVWzOtvz1Us+Wsv3fWBz0rDKQLnWCEj5BIfEkjZJcmJY+GLmNrTKnxsCZ/HmUqMUMolm4Q8SWmNK1TtMZ6Nmw6tEP45zkWpSXxznY2mSl4Ipa8vj+DuT+id319Twwq28Qhc0jdj4rMRdV5OwDQfyqUFAK0kFSzNqJ72ntn6/HLfJBtKVHoRqB7xB+u4M7n8o/9LS4DLmSjk8tSOcvqvCSa+0CDqkFdQh+bm5wttAX4bpFv5Zyrj9HIImfvmfJZgDXm43PUeqz9FjtRlalh7dJfVwlwsE+MTq553sSkKSGYK1j4pwKB3JDwMiJP8m8AzoijZRhyFvqYFX4yw5Ykrftg2OHPJ16FUoMOlL4p6+cEgm+yUOKC+az++PhTjOuHJMHPAg+XkA==?iv=M2Vvnlu6pJcf6if1x7ef7g==",
-    #     "sig": "2e8eaef67f40796be09be5a77b0ea7707e496484355774c13c078fbda4adb172301308dfc6d6256a2d65c33e5716dfd8efb1e6704cf5457ee723901ca1c7534c",
-    # }
+    test_event_1 = {
+        "id": "test",
+        "pubkey": "c63c5b4e21b9b1ec6b73ad0449a6a8589f6bd8542cabd9e5de6ae474b28fe806",
+        "created_at": 1705189941,
+        "kind": 5100,
+        "tags": [
+            ["p", "89669b03bb25232f33192fdda77b8e36e3d3886e9b55b3c74b95091e916c8f98"],
+            ["encrypted"],
+        ],
+        "content": "k84/yUywhNvWhDUOhxC/dWIb+w5R6KybLl0BpykYcDqY0ky7sJC4rnbxfsKvMjh1zIexyQ75M+NRxqA/JnBEekPw93xu6TfqtH/tVThbyOWG0fn77ptM1aEPziI52kwbM0hrSbtsJxKEm9LfWvF7+s9yv4/pqIkYMiMZ56JqxG/XH+3DxXoa0ZJQr1GRBpfsb1h8WMA5EqmEOB/OSKyeA9J7/7/U4ONHcsBc0yTPCBJ6pnTlgF19scwAvGXw9F1sG9gc/65nzvsfjaQ4venFh0A9QpH9f4/02qyBnOTcmTs0RIT9etyEZQeBoJlJL9+Tu1OD6sp66hbZ6GDVWzOtvz1Us+Wsv3fWBz0rDKQLnWCEj5BIfEkjZJcmJY+GLmNrTKnxsCZ/HmUqMUMolm4Q8SWmNK1TtMZ6Nmw6tEP45zkWpSXxznY2mSl4Ipa8vj+DuT+id319Twwq28Qhc0jdj4rMRdV5OwDQfyqUFAK0kFSzNqJ72ntn6/HLfJBtKVHoRqB7xB+u4M7n8o/9LS4DLmSjk8tSOcvqvCSa+0CDqkFdQh+bm5wttAX4bpFv5Zyrj9HIImfvmfJZgDXm43PUeqz9FjtRlalh7dJfVwlwsE+MTq553sSkKSGYK1j4pwKB3JDwMiJP8m8AzoijZRhyFvqYFX4yw5Ykrftg2OHPJ16FUoMOlL4p6+cEgm+yUOKC+az++PhTjOuHJMHPAg+XkA==?iv=M2Vvnlu6pJcf6if1x7ef7g==",
+        "sig": "2e8eaef67f40796be09be5a77b0ea7707e496484355774c13c078fbda4adb172301308dfc6d6256a2d65c33e5716dfd8efb1e6704cf5457ee723901ca1c7534c",
+    }
     #
     # # connect to db
-    # mongo_client = MongoClient(os.getenv("MONGO_URI"), tls=True)
-    # db = mongo_client["dvmdash"]
-    #
-    # # result = db["events"].create_index([("id", 1)], unique=True)
-    #
-    # # insert event
-    # result = db["events"].insert_one(test_event_1)
-    #
-    # print(result)
+    mongo_client = MongoClient(os.getenv("MONGO_URI"), tls=True)
+    db = mongo_client["dvmdash"]
 
-    run_nostr_client()
+    # result = db["events"].create_index([("id", 1)], unique=True)
+
+    # insert event
+    result = db["events"].insert_one(test_event_1)
+
+    print(result)
+
+    #run_nostr_client()
