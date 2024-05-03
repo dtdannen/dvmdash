24bd92416af5f99bb67f0b9f92c3f37c7cb83ff3
Dustin Dannenhauer <dustin.td@gmail.com>
2024-05-02 20:48:45 -0400

    revised run_and_monitor_listener.sh

--- scripts/run_and_monitor_listener.sh
+++ scripts/run_and_monitor_listener.sh
@@ -2,17 +2,20 @@
 
 script_path="listen_for_DVM_events.py"
 log_file="listen_for_DVM_events.log"
+virtualenv_path="../venv/"
 
-trap 'echo "Keyboard interrupt received. Exiting..."; exit 0' INT
+source "$virtualenv_path/bin/activate"
+
+trap 'echo "Keyboard interrupt received. Exiting..."; deactivate; exit 0' INT
 
 while true; do
-    echo "Starting script at $(date)" >> "$log_file"
-    python "$script_path" >> "$log_file" 2>&1
+    echo "Starting script at $(date)" | tee -a "$log_file"
+    python "$script_path"
 
     if [ $? -eq 0 ]; then
-        echo "Script completed successfully at $(date)" >> "$log_file"
+        echo "Script completed successfully at $(date)" | tee -a "$log_file"
     else
-        echo "Script died at $(date). Restarting in 5 seconds..." >> "$log_file"
+        echo "Script died at $(date). Restarting in 5 seconds..." | tee -a "$log_file"
         sleep 5
     fi
 done
