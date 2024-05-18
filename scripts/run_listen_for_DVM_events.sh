94410eb929bcf8ee182deab55d742b5716090731
Dustin Dannenhauer <dustin.td@gmail.com>
2024-05-18 17:16:31 -0400

    revising script for chron job

--- scripts/run_listen_for_DVM_events.sh
+++ scripts/run_listen_for_DVM_events.sh
@@ -20,7 +20,7 @@ then
 fi
 
 echo "[$(date)] Generating random values..."
-RUNTIME_LIMIT=$(shuf -i 5-20 -n 1)
+RUNTIME_LIMIT=$(shuf -i 2-3 -n 1)
 LOOKBACK_TIME=$(shuf -i 120-480 -n 1)
 WAIT_LIMIT=$(shuf -i 2-15 -n 1)
 WAIT_LIMIT=$((WAIT_LIMIT * 60))
