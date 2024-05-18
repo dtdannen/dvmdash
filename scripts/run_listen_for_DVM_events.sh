3216ebf43e4271ecd764af74207cb1ed56925cce
Dustin Dannenhauer <dustin.td@gmail.com>
2024-05-18 17:13:19 -0400

    revising script for chron job

--- scripts/run_listen_for_DVM_events.sh
+++ scripts/run_listen_for_DVM_events.sh
@@ -10,24 +10,24 @@ mkdir -p $LOG_DIR
 
 # Set the working directory
 cd /home/dvmdash/dvmdash || exit
+echo "[$(date)] Working directory set to: $(pwd)"
 
-{
-    echo "[$(date)] Checking if script is already running..."
-    if pgrep -f "$SCRIPT" > /dev/null
-    then
-        echo "[$(date)] Script is already running. Exiting."
-        exit 1
-    fi
+echo "[$(date)] Checking if script is already running..."
+if pgrep -f "$SCRIPT" > /dev/null
+then
+    echo "[$(date)] Script is already running. Exiting."
+    exit 1
+fi
 
-    echo "[$(date)] Generating random values..."
-    RUNTIME_LIMIT=$(shuf -i 5-20 -n 1)
-    LOOKBACK_TIME=$(shuf -i 120-480 -n 1)
-    WAIT_LIMIT=$(shuf -i 2-15 -n 1)
-    WAIT_LIMIT=$((WAIT_LIMIT * 60))
+echo "[$(date)] Generating random values..."
+RUNTIME_LIMIT=$(shuf -i 5-20 -n 1)
+LOOKBACK_TIME=$(shuf -i 120-480 -n 1)
+WAIT_LIMIT=$(shuf -i 2-15 -n 1)
+WAIT_LIMIT=$((WAIT_LIMIT * 60))
 
-    echo "[$(date)] Random values generated: RUNTIME_LIMIT=$RUNTIME_LIMIT, LOOKBACK_TIME=$LOOKBACK_TIME, WAIT_LIMIT=$WAIT_LIMIT"
-    echo "[$(date)] Activating virtual environment..."
-} >> /home/dvmdash/dvmdash/logs/cron_output.log
+echo "[$(date)] Random values generated: RUNTIME_LIMIT=$RUNTIME_LIMIT, LOOKBACK_TIME=$LOOKBACK_TIME, WAIT_LIMIT=$WAIT_LIMIT"
+echo "[$(date)] Activating virtual environment..."
+
 
 # Activate the virtual environment
 if [ -f "$VENV_PATH/bin/activate" ]; then
@@ -38,16 +38,16 @@ else
 fi
 
 # Run the Python script and log the output
-{
-    echo "[$(date)] Running the Python script..."
-    timeout $RUNTIME_LIMIT python $SCRIPT $RUNTIME_LIMIT $LOOKBACK_TIME $WAIT_LIMIT &> $LOG_FILE
 
-    if [ $? -ne 0 ]; then
-        echo "[$(date)] Python script encountered an error."
-    else
-        echo "[$(date)] Python script completed successfully."
-    fi
+echo "[$(date)] Running the Python script..."
+timeout $RUNTIME_LIMIT python $SCRIPT $RUNTIME_LIMIT $LOOKBACK_TIME $WAIT_LIMIT &> $LOG_FILE
 
-    echo "[$(date)] Deactivating virtual environment..."
-    deactivate
-} >> /home/dvmdash/dvmdash/logs/cron_output.log
+if [ $? -ne 0 ]; then
+    echo "[$(date)] Python script encountered an error."
+else
+    echo "[$(date)] Python script completed successfully."
+fi
+
+echo "[$(date)] Deactivating virtual environment..."
+deactivate
+
