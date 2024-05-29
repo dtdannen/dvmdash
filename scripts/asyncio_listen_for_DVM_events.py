2a823ec6913bea8b3750d5ea0bbfae48b35da63d
Dustin Dannenhauer <dustin.td@gmail.com>
2024-05-29 14:03:23 -0400

    first draft of loading data into neo4j database

---
+++ scripts/asyncio_listen_for_DVM_events.py
@@ -0,0 +1,293 @@
+import random
+import sys
+from datetime import datetime, timedelta
+import nostr_sdk
+import pymongo
+from pymongo import MongoClient
+import json
+import os
+import time
+from pathlib import Path
+from threading import Thread, Lock
+import ctypes
+import loguru
+import dotenv
+from nostr_sdk import (
+    Keys,
+    Client,
+    Tag,
+    EventBuilder,
+    Filter,
+    HandleNotification,
+    Timestamp,
+    nip04_decrypt,
+    LogLevel,
+    NostrSigner,
+    Kind,
+    SubscribeAutoCloseOptions,
+    Options,
+    Event,
+)
+from pymongo.errors import BulkWriteError
+from general.dvm import EventKind
+
+
+def setup_logging():
+    # Create a logs directory if it doesn't exist
+    log_dir = "logs"
+    if not os.path.exists(log_dir):
+        os.makedirs(log_dir)
+
+    # Configure the logger to use the DEBUG level
+    loguru.logger.remove()  # Remove the default logger
+    loguru.logger.add(sys.stderr, level="DEBUG")
+
+    # Init nostr_sdk logger
+    nostr_sdk.init_logger(LogLevel.DEBUG)
+
+    return loguru.logger
+
+
+# set up logging
+
+LOGGER = setup_logging()
+LOGGER.info("Starting up in current directory: ", os.getcwd())
+
+
+def setup_environment():
+    env_path = Path(".env")
+    if env_path.is_file():
+        LOGGER.info(f"Loading environment from {env_path.resolve()}")
+        dotenv.load_dotenv(env_path, verbose=True, override=True)
+    else:
+        LOGGER.error(f".env file not found at {env_path} ")
+        raise FileNotFoundError(f".env file not found at {env_path} ")
+
+
+setup_environment()
+
+
+def setup_database():
+    LOGGER.debug("os.getenv('USE_MONGITA', False): ", os.getenv("USE_MONGITA", False))
+
+    if (
+        os.getenv("USE_MONGITA", "False") != "False"
+    ):  # use a local mongo db, like sqlite
+        LOGGER.debug(
+            "os.getenv('USE_MONGITA', False): ", os.getenv("USE_MONGITA", False)
+        )
+        LOGGER.info("Using mongita")
+        from mongita import MongitaClientDisk
+
+        mongo_client = MongitaClientDisk()
+        db = mongo_client.dvmdash
+        LOGGER.info("Connected to local mongo db using MONGITA")
+    else:
+        # connect to db
+        mongo_client = MongoClient(os.getenv("MONGO_URI"), tls=True)
+        db = mongo_client["dvmdash"]
+
+        try:
+            result = db["events"].count_documents({})
+            LOGGER.info(f"There are {result} documents in events collection")
+        except Exception as e:
+            LOGGER.error("Could not count documents in db")
+            import traceback
+
+            traceback.print_exc()
+
+        LOGGER.info("Connected to cloud mongo db")
+
+    return db
+
+
+DB = setup_database()
+
+
+def get_relays():
+    relays = os.getenv(
+        "RELAYS",
+        "wss://relay.damus.io,wss://blastr.f7z.xyz,wss://relayable.org,wss://nostr-pub.wellorder.net",
+    ).split(",")
+    return relays
+
+
+RELAYS = get_relays()
+
+
+def get_relevant_kinds():
+    # Kinds to listen to
+    known_kinds = [
+        EventKind.DM.value,
+        EventKind.ZAP.value,
+        EventKind.DVM_NIP89_ANNOUNCEMENT.value,
+        EventKind.DVM_FEEDBACK.value,
+    ]
+
+    all_kinds = set(
+        known_kinds
+        + list(
+            range(
+                EventKind.DVM_RANGE_START.value,
+                EventKind.DVM_RANGE_END.value,
+            )
+        )
+        + list(
+            range(
+                EventKind.DVM_FEEDBACK_RANGE_START.value,
+                EventKind.DVM_FEEDBACK_RANGE_END.value,
+            )
+        )
+    )
+
+    relevant_kinds = [
+        Kind(k) for k in list(all_kinds - set(EventKind.get_bad_dvm_kinds()))
+    ]
+    return relevant_kinds
+
+
+# ugly, but we want it to be global, and only set once
+RELEVANT_KINDS = get_relevant_kinds()
+
+
+def write_events_to_db(events):
+    if events:
+        try:
+            result = DB["events"].insert_many(events, ordered=False)
+            LOGGER.info(
+                f"Finished writing events to db with result: {len(result.inserted_ids)}"
+            )
+        except BulkWriteError as e:
+            # If you want to log the details of the duplicates or other errors
+            num_duplicates_found = len(e.details["writeErrors"])
+            LOGGER.warning(
+                f"Ignoring {num_duplicates_found} / {len(events)} duplicate events...",
+                end="",
+            )
+        except Exception as e:
+            LOGGER.error(f"Error inserting events into database: {e}")
+
+
+class NotificationHandler(HandleNotification):
+    def __init__(self):
+        self.events = []
+        self.lock = Lock()
+        self.flush_interval = 10  # Flush every 10 seconds, adjust as needed
+        self.stop_requested = False  # Flag to signal thread to stop
+
+        # Start a thread to flush events periodically
+        self.flush_thread = Thread(target=self.flush_events_periodically)
+        self.flush_thread.start()
+
+    def handle(self, relay_url, subscription_id, event: Event):
+        if event.kind() in RELEVANT_KINDS:
+            with self.lock:
+                self.events.append(json.loads(event.as_json()))
+
+    def flush_events(self):
+        with self.lock:
+            if self.events:
+                LOGGER.debug("locking to write to db...", end="")
+                LOGGER.debug(f"...writing {len(self.events)} to db...", end="")
+                write_events_to_db(self.events)
+                self.events.clear()
+                LOGGER.debug("...unlocking write to db")
+
+    def flush_events_periodically(self):
+        while not self.stop_requested:
+            time.sleep(self.flush_interval)
+            self.flush_events()
+
+    def request_stop(self):
+        self.stop_requested = True
+
+    def handle_msg(self, relay_url, msg):
+        return
+
+
+def nostr_client(since_when_timestamp: Timestamp, runtime_limit: Timestamp):
+    keys = Keys.generate()
+    pk = keys.public_key()
+    LOGGER.info(f"Nostr Test Client public key: {pk.to_bech32()}, Hex: {pk.to_hex()} ")
+    signer = NostrSigner.keys(keys)
+    client = Client(signer)
+    for relay in RELAYS:
+        client.add_relay(relay)
+    client.connect()
+
+    dvm_filter = Filter().kinds(RELEVANT_KINDS).since(since_when_timestamp)
+    client.subscribe([dvm_filter], None)
+
+    handler = NotificationHandler()
+    client.handle_notifications(handler)
+
+    while Timestamp.now().as_secs() < runtime_limit.as_secs():
+        LOGGER.debug(
+            f"There are this many seconds left: {runtime_limit.as_secs() - Timestamp.now().as_secs()}"
+        )
+        delay = 10
+        LOGGER.debug(f"About to sleep for {delay} seconds...", end="")
+        time.sleep(delay)
+        LOGGER.debug(f"waking up...")
+
+    LOGGER.info("Time is up. Requesting stop.")
+    client.disconnect()
+    handler.request_stop()
+
+
+def run_nostr_client(run_time_limit_minutes=10, look_back_minutes=120):
+    current_timestamp = Timestamp.now()
+    current_secs = current_timestamp.as_secs()
+
+    max_run_time = Timestamp.from_secs(current_secs + int(run_time_limit_minutes * 60))
+    look_back_time = Timestamp.from_secs(current_secs - int(look_back_minutes * 60))
+
+    env_path = Path(".env")
+    if env_path.is_file():
+        LOGGER.info(f"loading environment from {env_path.resolve()}")
+        dotenv.load_dotenv(env_path, verbose=True, override=True)
+    else:
+        LOGGER.error(f".env file not found at {env_path} ")
+        raise FileNotFoundError(f".env file not found at {env_path} ")
+
+    nostr_dvm_thread = Thread(target=nostr_client, args=(look_back_time, max_run_time))
+    nostr_dvm_thread.start()
+    nostr_dvm_thread.join()  # Wait for the thread to finish
+
+
+if __name__ == "__main__":
+    # get the limits from sys.args
+    if len(sys.argv) == 4:
+        RUNTIME_LIMIT = int(sys.argv[1])
+        LOOKBACK_TIME = int(sys.argv[2])
+        WAIT_LIMIT = int(sys.argv[3])
+    else:
+        print(
+            "Usage: python listen_for_DVM_events.py <RUNTIME_LIMIT_AS_MINS>"
+            " <LOOKBACK_TIME_AS_MINS> <WAIT_LIMIT_AS_SECS>\n"
+            "Example: python listen_for_DVM_events.py 10 120 60\n"
+        )
+        sys.exit(1)
+
+    try:
+        LOGGER.info(f"Starting client run with RUNTIME_LIMIT: {RUNTIME_LIMIT} minutes")
+        LOGGER.info(f"Starting client run with LOOKBACK_TIME: {LOOKBACK_TIME} minutes")
+        LOGGER.info(f"Starting client run with WAIT_LIMIT: {WAIT_LIMIT} seconds")
+
+        run_nostr_client(
+            RUNTIME_LIMIT, LOOKBACK_TIME
+        )  # Replace 3 with your desired run time limit in minutes
+        LOGGER.info(
+            f"Client run completed. Sleeping for {WAIT_LIMIT} seconds before exiting completely"
+        )
+        time.sleep(WAIT_LIMIT)  # Sleep for a short time before restarting
+        LOGGER.info("Goodbye!")
+    except Exception as e:
+        LOGGER.error(f"Exception occurred: {e}")
+        LOGGER.info(
+            f"Client exception occurred. Sleeping for {WAIT_LIMIT} seconds before exiting completely"
+        )
+        time.sleep(
+            WAIT_LIMIT
+        )  # Sleep for a short time before restarting in case of an exception
+        LOGGER.info("Goodbye!")
--- scripts/initialize_neo4j_db.py
+++ scripts/initialize_neo4j_db.py
@@ -3,17 +3,26 @@ This script listens for updates to mongodb and triggers a function to update the
 """
 
 from pymongo import MongoClient
-from bson.codec_options import CodecOptions
-from datetime import datetime, timedelta
-import statistics
 import loguru
 import os
 import sys
-from neo4j import GraphDatabase
+from neo4j import (
+    GraphDatabase,
+    basic_auth,
+    TrustCustomCAs,
+    TrustSystemCAs,
+    TRUST_SYSTEM_CA_SIGNED_CERTIFICATES,
+)
+from neo4j.exceptions import ServiceUnavailable
 import json
 import general.helpers as helpers
 from tqdm import tqdm
 import hashlib
+from pathlib import Path
+import dotenv
+import ssl
+
+import certifi
 
 
 def setup_logging():
@@ -35,11 +44,40 @@ logger = setup_logging()
 logger.info("Starting up in current directory: ", os.getcwd())
 
 
+def setup_environment():
+    env_path = Path(".env")
+    if env_path.is_file():
+        try:
+            dotenv.load_dotenv(env_path, verbose=True, override=True)
+            logger.info(f"Loaded environment from {env_path.resolve()}")
+        except Exception as e:
+            logger.error(f"Error loading environment from {env_path.resolve()}")
+            logger.error(e)
+            sys.exit(1)
+    else:
+        logger.error(f".env file not found at {env_path} ")
+        raise FileNotFoundError(f".env file not found at {env_path} ")
+
+
+setup_environment()
+
+
 def setup_databases():
-    logger.debug("os.getenv('USE_MONGITA', False): ", os.getenv("USE_MONGITA", False))
+    if os.getenv("USE_MONGITA", None) is None:
+        logger.warning(
+            "USE_MONGITA is not set. Check that the env file is loading correctly."
+        )
+
+    if os.getenv("MONGO_URI", None) is None:
+        logger.error("MONGO_URI is not set. Exiting")
+        sys.exit(1)
+
+    # This gets the path to the file containing trusted CA certificates.
+    # This was needed to get this to work on a local mac, it may not be needed on a server linux machine
+    ca = certifi.where()
 
     # connect to db
-    mongo_client = MongoClient(os.getenv("MONGO_URI"), tls=True)
+    mongo_client = MongoClient(os.getenv("MONGO_URI"), tls=True, tlsCAFile=ca)
     db = mongo_client["dvmdash"]
 
     try:
@@ -50,14 +88,22 @@ def setup_databases():
         import traceback
 
         traceback.print_exc()
+        sys.exit(1)
 
     logger.info("Connected to cloud mongo db")
 
-    uri = os.getenv("NEO4J_URI")
-    user = os.getenv("NEO4J_USERNAME")
-    password = os.getenv("NEO4J_PASSWORD")
+    URI = os.getenv("NEO4J_URI")
+    AUTH = (os.getenv("NEO4J_USERNAME"), os.getenv("NEO4J_PASSWORD"))
 
-    neo4j_driver = GraphDatabase.driver(uri, auth=(user, password))
+    neo4j_driver = GraphDatabase.driver(
+        URI,
+        auth=AUTH,
+        # encrypted=True,
+        # trust=TRUST_SYSTEM_CA_SIGNED_CERTIFICATES,
+    )
+
+    neo4j_driver.verify_connectivity()
+    logger.info("Verified connectivity to Neo4j")
 
     return db, neo4j_driver
 
@@ -112,7 +158,7 @@ def create_npub(tx, npub_hex: str, npub: str, name: str = "") -> None:
 # Function to create a hash of the content string
 def generate_content_hash(content: str) -> str:
     """
-    Generate a SHA-256 hash for the given content string.
+    Generate an SHA-256 hash for the given content string.
 
     Parameters:
     content (str): The content string to be hashed.
@@ -124,13 +170,16 @@ def generate_content_hash(content: str) -> str:
 
 
 # Function to create a content node if it does not already exist
-def create_content(tx, content: str, max_size: int = 1000) -> None:
+def create_content(
+    tx, content: str, content_orig_event_id: str, max_size: int = 1000
+) -> None:
     """
     Create a node in the Neo4j database with label 'Content' using a hash of the content to prevent duplicates.
 
     Parameters:
     tx (Transaction): The Neo4j transaction context.
     content (str): The content string.
+    content_orig_event_id (str): The id of the event that has the content
     max_size (int): The maximum allowed size for the content in bytes.
 
     Returns:
@@ -141,28 +190,27 @@ def create_content(tx, content: str, max_size: int = 1000) -> None:
 
     # Check if the content size is within the allowed limit
     if byte_size <= max_size:
-        # Generate a unique content_id using the hash of the content
-        content_id = generate_content_hash(content)
-
         # Define the Cypher query to create a node with the given properties only if it does not exist
         query = """
-        MERGE (c:Content {content_id: $content_id})
+        MERGE (c:Content {content_id: $content_orig_event_id})
         ON CREATE SET c.content = $content
         """
-        tx.run(query, content_id=content_id, content=content)
-        print(f"Content node created or already exists: {content_id}")
+        tx.run(query, content_orig_event_id=content_orig_event_id, content=content)
+        print(f"Content node created or already exists: {content_orig_event_id}")
     else:
         print(f"Content too large to create node: size: {byte_size} bytes")
 
 
-def create_created_for_relationship(tx, npub_hex: str, content_hash: str) -> None:
+def create_created_for_relationship(
+    tx, npub_hex: str, content_orig_event_id: str
+) -> None:
     """
     Create a relationship in the Neo4j database between an NPub node and a Content node.
 
     Parameters:
     tx (Transaction): The Neo4j transaction context.
     npub_hex (str): The hexadecimal representation of the public key.
-    content_hash (str): The SHA-256 hash of the content.
+    content_orig_event_id (str): The id of the event that has the content
 
     Returns:
     None
@@ -170,18 +218,20 @@ def create_created_for_relationship(tx, npub_hex: str, content_hash: str) -> Non
     # Define the Cypher query to create a relationship between the NPub and Content nodes
     query = """
     MATCH (n:NPub {npub_hex: $npub_hex})
-    MATCH (c:Content {content_id: $content_id})
-    MERGE (n)-[:CREATED_FOR]->(c)
+    MATCH (c:Content {content_id: $content_orig_event_id})
+    MERGE (c)-[:CREATED_FOR]->(n)
     """
 
     # Execute the query with the provided parameters
-    tx.run(query, npub_hex=npub_hex, content_id=content_hash)
+    tx.run(query, npub_hex=npub_hex, content_orig_event_id=content_orig_event_id)
 
 
 def process_notes_into_neo4j(mongo_db, neo4j_driver):
     all_dvm_request_events = list(
         mongo_db.events.find({"kind": {"$gte": 5000, "$lte": 5999}})
     )
+
+    # remove 5666
     all_dvm_request_events = [
         event for event in all_dvm_request_events if event["kind"] not in [5666]
     ]
@@ -190,9 +240,12 @@ def process_notes_into_neo4j(mongo_db, neo4j_driver):
     all_dvm_response_events = list(
         mongo_db.events.find({"kind": {"$gte": 6000, "$lte": 6999}})
     )
+
+    # remove 6666
     all_dvm_response_events = [
         event for event in all_dvm_response_events if event["kind"] not in [6666]
     ]
+
     logger.info(f"Loaded {len(all_dvm_response_events)} dvm response events from mongo")
 
     dvm_nip89_profiles = get_all_dvm_nip89_profiles(mongo_db)
@@ -200,15 +253,15 @@ def process_notes_into_neo4j(mongo_db, neo4j_driver):
 
     with neo4j_driver.session() as session:
         # first, create all nodes for all npubs
-        for event in tqdm(all_dvm_request_events + all_dvm_response_events):
-            npub_hex = event["pubkey"]
-            npub = helpers.hex_to_npub(npub_hex)
-
-            name = ""
-            if npub_hex in dvm_nip89_profiles:
-                name = dvm_nip89_profiles[npub_hex].get("name", "")
-
-            create_npub(session, npub_hex, npub, name)
+        # for event in tqdm(all_dvm_request_events + all_dvm_response_events):
+        #     npub_hex = event["pubkey"]
+        #     npub = helpers.hex_to_npub(npub_hex)
+        #
+        #     name = ""
+        #     if npub_hex in dvm_nip89_profiles:
+        #         name = dvm_nip89_profiles[npub_hex].get("name", "")
+        #
+        #     create_npub(session, npub_hex, npub, name)
 
         # second, create content nodes for all DVM response events and create relationship between
         for event in tqdm(all_dvm_response_events):
@@ -219,18 +272,50 @@ def process_notes_into_neo4j(mongo_db, neo4j_driver):
                 continue
 
             content_payload_str = event["content"]
+            content_orig_event_id = event["id"]
 
-            create_content(session, content_payload_str)
+            create_content(session, content_payload_str, content_orig_event_id)
 
             customer_npub_hex = next(
                 (tag[1] for tag in event["tags"] if tag[0] == "p"), None
             )
 
+            if customer_npub_hex is None:
+                # try to get it from the request tag
+                request_tag_content = next(
+                    (
+                        req_tag[1]
+                        for req_tag in event["tags"]
+                        if req_tag[0] == "request"
+                    ),
+                    None,
+                )
+
+                if request_tag_content:
+                    request_tag_content = json.loads(request_tag_content)
+                    if "pubkey" in request_tag_content:
+                        customer_npub_hex = request_tag_content["pubkey"]
+                        logger.warning(
+                            "Got customer npub from request tag because it was not in the 'p' tag"
+                        )
+            else:
+                logger.info("Got customer npub from 'p' tag")
+                pass
+
+            if customer_npub_hex is None:
+                logger.warning(
+                    f"Event {event['id']} for kind {event['kind']} does not have a customer npub"
+                )
+                continue
+
             if customer_npub_hex:
                 create_created_for_relationship(
                     session,
-                    helpers.hex_to_npub(customer_npub_hex),
-                    generate_content_hash(content_payload_str),
+                    customer_npub_hex,
+                    content_orig_event_id,
+                )
+                logger.info(
+                    f"A relationship was created between {helpers.hex_to_npub(customer_npub_hex)} and {content_orig_event_id}"
                 )
 
 
