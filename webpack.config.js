45abfba112a40892e25cbad911631497c9cc4874
Dustin Dannenhauer <dustin.td@gmail.com>
2024-08-23 03:02:56 +0300

    still broken, working on getting react to work

--- webpack.config.js
+++ webpack.config.js
@@ -1,30 +1,37 @@
 const path = require('path');
+const webpack = require('webpack');
 
 module.exports = {
-  entry: './src/index.js',
+  entry: './src/index.js',  // adjust this to your entry point
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dvmdash/monitor/static/js'),
   },
   module: {
-      rules: [
-        {
-          test: /\.(js|jsx)$/,
-          exclude: /node_modules/,
-          use: {
-            loader: 'babel-loader',
-            options: {
-              presets: ['@babel/preset-env', '@babel/preset-react']
-            }
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react']
           }
-        },
-        {
-          test: /\.css$/,
-          use: ['style-loader', 'css-loader']
         }
-      ]
-    },
-    resolve: {
-      extensions: ['.js', '.jsx']
-    }
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['.js', '.jsx']
+  },
+  plugins: [
+    new webpack.ProvidePlugin({
+      React: 'react',
+      ReactDOM: 'react-dom'
+    })
+  ]
 };
