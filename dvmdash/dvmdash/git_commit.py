6aecec5f84b6f1141110b56d1695dffc3b1555ec
Dustin Dannenhauer <dustin.td@gmail.com>
2024-09-02 01:09:18 -0400

    Implement dynamic retrieval of GIT_COMMIT using subprocess to fetch current commit hash or default to 'unknown'.

--- dvmdash/dvmdash/git_commit.py
+++ dvmdash/dvmdash/git_commit.py
@@ -1,1 +1,9 @@
-GIT_COMMIT = 'unknown'
+import subprocess
+
+def get_git_commit():
+    try:
+        return subprocess.check_output(['git', 'rev-parse', '--short', 'HEAD']).decode('ascii').strip()
+    except:
+        return 'unknown'
+
+GIT_COMMIT = get_git_commit()
