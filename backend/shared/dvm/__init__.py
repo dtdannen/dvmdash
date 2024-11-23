1dd1a88b441323730f63db686404edd935ea128c
Dustin Dannenhauer <dustin.td@gmail.com>
2024-11-22 19:21:48 -0500

    creating a dvm_kinds.yaml file

---
+++ backend/shared/dvm/__init__.py
@@ -0,0 +1,17 @@
+from .kinds import (
+    EventKind,
+    get_relevant_kinds,
+    get_excluded_kinds,
+    is_dvm_kind,
+    is_dvm_request,
+    is_dvm_result,
+)
+
+__all__ = [
+    "EventKind",
+    "get_relevant_kinds",
+    "get_excluded_kinds",
+    "is_dvm_kind",
+    "is_dvm_request",
+    "is_dvm_result",
+]
